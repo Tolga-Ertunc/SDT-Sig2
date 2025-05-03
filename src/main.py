1225b6fddb17ba9466e48e8cc3a8ad443c178a64
Tolga Ertunc <tolga@Tolgas-MacBook-Air.local>
2025-05-03 16:35:25 +0200

    Added list creation code

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 def main():
     print("GIT Assignment Project-SDT")
+    
+    # Creating a basic list
+    my_list = [0] * 10
+    print(f"List created, size: {len(my_list)}")
 
 if __name__ == "__main__":
     main()
