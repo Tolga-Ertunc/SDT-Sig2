8504cd7101c52a5e12f8acd4cda3a66814447c46
Tolga Ertunc <tolga@Tolgas-MacBook-Air.local>
2025-05-03 16:59:08 +0200

    List initialized with some random values

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
+import random
+
 def main():
     print("GIT Assignment Project-SDT")
     
-    # Creating a basic list
+    # Creating a list
     my_list = [0] * 10
     print(f"List created, size: {len(my_list)}")
+    
+    # Adding some random values for task G
+    for i in range(len(my_list)):
+        my_list[i] = random.randint(1, 100)
+        print(f"my_list[{i}] = {my_list[i]}")
 
 if __name__ == "__main__":
     main()
