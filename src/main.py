115a395fb049ec3a85c80b6bd25c2c0a90013ca3
Tolga Ertunc <tolga@Tolgas-MacBook-Air.local>
2025-05-03 17:04:06 +0200

    Added list sorting code

--- src/main.py
+++ src/main.py
@@ -7,10 +7,22 @@ def main():
     my_list = [0] * 10
     print(f"List created, size: {len(my_list)}")
     
-    # Adding some random values for task G
+    # Filling list with some random values
     for i in range(len(my_list)):
         my_list[i] = random.randint(1, 100)
         print(f"my_list[{i}] = {my_list[i]}")
+    
+    # Sorting list elements for task I/i    
+    print("\nBefore sorting:")
+    print_list(my_list)
+    
+    my_list.sort()
+    
+    print("\nAfter sorting:")
+    print_list(my_list)
+
+def print_list(lst):
+    print(' '.join(map(str, lst)))
 
 if __name__ == "__main__":
     main()
